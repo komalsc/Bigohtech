730c9d6f37397ef2f94a23e1c6699d8d2d09e281
komalsc <komalchauhan00022@gmail.com>
2023-05-15 18:50:39 +0530

    changes Co-authored-by: Virender121 <virender121@users.noreply.github.com>

--- src/Card1.jsx
+++ src/Card1.jsx
@@ -7,12 +7,32 @@ function Card1() {
       <Card.Img variant="top"  src="https://profileimagepaleo.s3.amazonaws.com/companycover/669_1669885274521_File%20name" />
       <img class="avatar-img rounded border border-4 border-card" alt="..." src="https://profileimagepaleo.s3.amazonaws.com/companyprofile/669_1669885175528_File%20name"></img>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Card.Title>Big Oh Notation</Card.Title>
+         <p>.Innovate .Transparent .Impact
+          Software</p>
+        <Button variant="primary">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+        </Button>
       </Card.Body>
     </Card>
   );
